270a8c45386ae5ad679e44d787abf31ac508fd6d
arvindpndit <arvindpandit9808@gmail.com>
2024-05-04 17:01:03 +0530

    add Navbar and Searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar />
+      <SearchBar />
+    </div>
+  );
+};
 
-export default App
+const Navbar = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "20px",
+        alignItems: "center",
+        backgroundColor: "blue",
+      }}
+    >
+      <h2>Prime video</h2>
+      <SearchBar />
+      <div>Avatar</div>
+    </div>
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" placeholder="search videos/series...."></input>
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default App;
