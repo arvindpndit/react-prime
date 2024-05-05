ea2e8378434b0e12ad0921540fd157fabf5f663a
arvindpndit <arvindpandit9808@gmail.com>
2024-05-05 16:56:01 +0530

    style navbar using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <SearchBar />
     </div>
   );
 };
 
 const Navbar = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: "20px",
-        alignItems: "center",
-        backgroundColor: "blue",
-      }}
-    >
-      <h2>Prime video</h2>
+    <div className="flex p-4 border rounded-2xl mt-2 bg-gray-50 hover:bg-gray-100 container max-w-screen-lg mx-auto justify-between items-center ">
+      <h2 className="text-xl text-gray-700 font-semibold">Prime video</h2>
       <SearchBar />
-      <div>Avatar</div>
+      <img
+        className="h-12 rounded-full"
+        src="https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJaaFhuNmhEV0lBb1Jva3B4U0pWb1VJdWF6MCJ9"
+      ></img>
     </div>
   );
 };
 
 const SearchBar = () => {
   return (
-    <div>
-      <input type="text" placeholder="search videos/series...."></input>
-      <button>Search</button>
+    <div className="flex items-center">
+      <input
+        className="border border-gray-300 rounded-l px-4 py-2 focus:outline-none focus:border-blue-500"
+        type="text"
+        placeholder="Search videos/series...."
+      />
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-r">
+        Search
+      </button>
     </div>
   );
 };
