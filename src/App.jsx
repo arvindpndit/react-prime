cad616aaea0b038d38939c3b547cf10713114958
arvindpndit <arvindpandit9808@gmail.com>
2024-05-07 15:40:59 +0530

    useState hook - counter demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 const App = () => {
+  //let counter = 0;
+  const [counter, setCounter] = useState(0);
+
+  const handleIncBtn = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
+  console.log("re-render");
   return (
-    <div>
+    <div className="p-10">
       <Navbar />
+      {counter}
+      <br></br>
+      <button onClick={handleIncBtn} className="p-1 bg-slate-300 rounded-xl">
+        Increase
+      </button>
     </div>
   );
 };
 
 export default App;
+
+//hooks are just js fn -  useState , useEffect ,useMemo, useId, ...
+// state variable are special variables that trigger a re-render when their value changes
