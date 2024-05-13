b611f41ebbd2fa1f141a2849c560adf96ed3b9da
arvindpndit <arvindpandit9808@gmail.com>
2024-05-13 10:55:14 +0530

    keys && props

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -1,4 +1,5 @@
 import useFetchMovies from "../hooks/useFetchMovies";
+import MovieCard from "./MovieCard";
 
 const MoviesContainer = () => {
   const movies = useFetchMovies();
@@ -6,21 +7,11 @@ const MoviesContainer = () => {
   return (
     <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {movies &&
-        movies.map((movie, index) => (
-          <div key={index} className="bg-white rounded-lg shadow-lg">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.original_title}
-              className="w-full h-64 object-cover rounded-t-lg"
-            />
-            <div className="p-4">
-              <h2 className="text-xl font-semibold">{movie.original_title}</h2>
-              <p className="text-gray-600">{movie.release_date}</p>
-            </div>
-          </div>
-        ))}
+        movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
     </div>
   );
 };
 
 export default MoviesContainer;
+
+//keys - special attribute
